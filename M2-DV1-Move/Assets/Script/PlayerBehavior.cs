b697e3e178c22a1951613447645b7f500e669f4f
linuxgaming25 <127547557+linuxgaming25@users.noreply.github.com>
2024-02-16 04:40:33 -0700

    Main Assets

---
+++ M2-DV1-Move/Assets/Script/PlayerBehavior.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+
+    public float distanceToGround = 0.1f;
+
+    public LayerMask groundLayer;
+
+    private float vInput;
+    private float hInput;
+    private Rigidbody _rb;
+
+    private CapsuleCollider _col;
+    
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
+
+    void Start()
+    {
+        // 3
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        /* 4
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
+
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+        }
+        
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+        
+        return grounded;
+    }
+}
