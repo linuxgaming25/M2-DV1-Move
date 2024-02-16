64761a27945600cc730217c1e87ef68d89cf30ea
linuxgaming25 <127547557+linuxgaming25@users.noreply.github.com>
2024-02-16 16:07:28 -0700

    change ground to default
    copy PlayerBehavior to old versions

--- M2-DV1-Move/Assets/Script/PlayerBehavior.cs
+++ M2-DV1-Move/Assets/Script/PlayerBehavior.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
     public float jumpVelocity = 5f;
+
+    public bool demoKinematicMovement = false;
 
     public float distanceToGround = 0.1f;
 
@@ -17,15 +20,20 @@ public class PlayerBehaviour : MonoBehaviour
     private Rigidbody _rb;
 
     private CapsuleCollider _col;
-    
+    private GameBehavior _gameManager;
+
+
     public GameObject bullet;
     public float bulletSpeed = 100f;
 
+    private bool doJump = false;
+    private bool doShoot = false;
+
     void Start()
     {
-        // 3
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<CapsuleCollider>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
     }
 
     void Update()
@@ -33,15 +41,32 @@ public class PlayerBehaviour : MonoBehaviour
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        /* 4
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
-        */
+        //if (demoKinematicMovement)
+        //{
+        //    MoveKinematically();
+        //}
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            doJump = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            doShoot = true;
+        }
+
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if(Input.GetMouseButtonDown(0))
+        //if (demoKinematicMovement)
+        //{
+        //    return;
+        //}
+
+
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
 
@@ -49,27 +74,54 @@ public class PlayerBehaviour : MonoBehaviour
 
             bulletRB.velocity = this.transform.forward * bulletSpeed;
         }
-        
-        Vector3 rotation = Vector3.up * hInput;
 
+        if (doJump)
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            doJump = false;
+            //playerJump();
+        }
+
+        Vector3 rotation = Vector3.up * hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if (doShoot)
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform.position + this.transform.right, this.transform.rotation) as GameObject;
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+            doShoot = false;
+        }
+
 
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
 
         _rb.MoveRotation(_rb.rotation * angleRot);
 
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
-        }
+        //if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        //}
     }
+    //void MoveKinematically()
+    //{
+    //    this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+    //    this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    //}
 
     private bool IsGrounded()
     {
         Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
-        
+
         bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
-        
+        //bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround);
+
+
+        //grounded = false;
+        Debug.Log("Grounded: " + _col.bounds.center + ", " + capsuleBottom + ", " + distanceToGround);
         return grounded;
     }
+
 }
